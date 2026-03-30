1bdb5746849e1b1c2965cb34977d942c0bbecea6
rj7075 <rj7075yadav@gmail.com>
2026-03-30 14:38:33 +0000

    watsapp added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,18 @@ import { Toaster } from "react-hot-toast";
 
 import "./index.css";
 import Brokers from "./BrokersPage/Pages/Brokers";
+import StickyWatsapp from "./BrokersPage/components/stickywatsapp";
 
 function App() {
   return (
 
     <>
+    <StickyWatsapp/>
     <Toaster position="top-right" reverseOrder={false} />
     <Routes>
       {/* Toast Container */}
        <Route path="/" element={<Brokers/>}/>
+       
     </Routes>
     </>
   );
